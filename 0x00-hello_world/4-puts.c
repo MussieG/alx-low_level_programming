479c98ab321cc74df48b80c44d17e53e2bf118ea
MussieG <mussie.gebregziabher27@gmail.com>
2021-06-17 13:22:55 -0700

    a C program that prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-   #include <stdio.h>
+#include <stdio.h>
 
-   /**
-    * main - Prints quote
-    *
-    * Return: zero on success
-    *
-    */
-   int main(void)
-   {
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
-   }
+/**
+ * main - Prints quote
+ *
+ * Return: zero on success
+ *
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
