4e7ce8ef30e3ab922966d2262ab3bceaffdbec93
MussieG <mussie.gebregziabher27@gmail.com>
2021-06-17 13:51:38 -0700

    a C program that prits exactly

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio>h>
+
+/**
+* main - Prints quote
+*
+* Return: zero on success
+*
+*/
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}
